25b89e34b1ee2ce1788d8f16bcefbd310aca67fa
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-18 21:29:25 +0530

    Create Length of the string.java

---
+++ Operators and Conditional Statements/Coding Practice 3/Length of the string.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        System.out.println(string.length());
+    }
+}
