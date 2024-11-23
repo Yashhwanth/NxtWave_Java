8342e5e3f544f5b0bd1a95334c220ffe602deefe
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-23 23:40:42 +0530

    Update Second Line.java

--- IO Basics/Coding Practice 1/Second Line.java
+++ IO Basics/Coding Practice 1/Second Line.java
@@ -1,5 +1,10 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        String str1 = input.nextLine();
+        String str2 = input.nextLine();
+        System.out.println(str2);
+    }
 }
