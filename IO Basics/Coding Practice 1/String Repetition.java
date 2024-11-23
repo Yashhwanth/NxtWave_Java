513fd7a08e80d7503bd98d212c968d590912a039
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-23 23:43:30 +0530

    Update String Repetition.java

--- IO Basics/Coding Practice 1/String Repetition.java
+++ IO Basics/Coding Practice 1/String Repetition.java
@@ -1,5 +1,10 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine() + " ";
+        Integer num = input.nextInt();
+        System.out.print(str.repeat(num));
+    }
 }
