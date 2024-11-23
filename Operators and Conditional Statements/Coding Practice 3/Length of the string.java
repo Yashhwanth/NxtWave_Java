f1bad9753816d8efebc3a281f67f614f6e8f21b5
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-23 23:11:56 +0530

    Delete Operators and Conditional Statements/Coding Practice 3 directory

--- Operators and Conditional Statements/Coding Practice 3/Length of the string.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        String string = input.nextLine();
-        System.out.println(string.length());
-    }
-}
