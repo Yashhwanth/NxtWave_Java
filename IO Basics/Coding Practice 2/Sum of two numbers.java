d567e16350c3c464fe9f5dcd29d8b5cdc19074be
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-25 07:56:52 +0530

    Update Sum of two numbers.java

--- IO Basics/Coding Practice 2/Sum of two numbers.java
+++ IO Basics/Coding Practice 2/Sum of two numbers.java
@@ -1,5 +1,9 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Integer num1 = input.nextInt();
+        Integer num2 = input.nextInt();
+        System.out.println(num1 + num2);
+    }
 }
