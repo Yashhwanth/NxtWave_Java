7f1f19b16121da61f468cbf712d06d5b052423f8
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-25 08:03:08 +0530

    Update Sum of the digits.java

--- IO Basics/Coding Practice 2/Sum of the digits.java
+++ IO Basics/Coding Practice 2/Sum of the digits.java
@@ -1,5 +1,8 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        System.out.println(Character.getNumericValue(str.charAt(0))+Character.getNumericValue(str.charAt(1))+Character.getNumericValue(str.charAt(2)));
+    }
 }
