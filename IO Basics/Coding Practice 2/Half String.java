bfcb14b4b6f7a9ad1607a94e08b90cc8e1901e94
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-25 08:00:16 +0530

    Update Half String.java

--- IO Basics/Coding Practice 2/Half String.java
+++ IO Basics/Coding Practice 2/Half String.java
@@ -1,5 +1,8 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        System.out.println(str.substring(0,str.length()/2));
+    }
 }
