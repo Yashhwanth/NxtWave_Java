7b5f29b9863dcf83245a55ac730882ce589e4239
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-27 22:53:53 +0530

    Update and rename Hello World - Copy (5).java to First & last digits.java

---
+++ Operators and Conditional Statements/Assignment - 1/First & last digits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(3));
+  
+    }
+}
