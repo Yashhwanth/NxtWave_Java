b830685ac85967394d4a977416d3791f61f2063e
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-27 22:59:04 +0530

    Update and rename Hello World - Copy (7).java to Women Population.java

---
+++ Operators and Conditional Statements/Assignment - 1/Women Population.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        //Float women_per = 48;
+        Scanner input = new Scanner(System.in);
+        Integer population = input.nextInt();
+        System.out.println((48*population)/100);
+    }
+}
