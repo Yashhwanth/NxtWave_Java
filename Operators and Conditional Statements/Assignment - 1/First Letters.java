2f3088b65811ea1e1c9c2b17ad1b967d56c2d4aa
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-27 22:50:12 +0530

    Update and rename Hello World - Copy (3).java to First Letters.java

---
+++ Operators and Conditional Statements/Assignment - 1/First Letters.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        String str1 = input.nextLine();
+        String str2 = input.nextLine();
+        String str3 = input.nextLine();
+        System.out.print(str1.charAt(0));
+        System.out.print(str2.charAt(0));
+        System.out.print(str3.charAt(0));
+
+    }
+}
