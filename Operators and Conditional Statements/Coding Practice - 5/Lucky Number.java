0600b2af4665ce8d25eb2dcae97ad2844e65fe1e
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 23:04:18 +0530

    Update Lucky Number.java

--- Operators and Conditional Statements/Coding Practice - 5/Lucky Number.java
+++ Operators and Conditional Statements/Coding Practice - 5/Lucky Number.java
@@ -1,5 +1,15 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int greater = num1 > num2 ? num1 : num2;
+        int smaller = num1 < num2 ? num1 : num2;
+        if ((num1 ==6 || num2 == 6) || ((num1 + num2) ==6 || (greater - smaller) == 6)){
+            System.out.println("Lucky");
+        }else{
+            System.out.println("Not Lucky");
+        }
+    }
 }
