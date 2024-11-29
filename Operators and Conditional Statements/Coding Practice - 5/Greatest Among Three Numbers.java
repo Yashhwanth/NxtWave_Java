12fb156dd52067bf66a4a97a3f2c78071d0921b3
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 23:05:00 +0530

    Update Greatest Among Three Numbers.java

--- Operators and Conditional Statements/Coding Practice - 5/Greatest Among Three Numbers.java
+++ Operators and Conditional Statements/Coding Practice - 5/Greatest Among Three Numbers.java
@@ -1,5 +1,18 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if(a > b && a>c){
+            System.out.println(a);
+        }else if( b>c){
+                    System.out.println(b);
+        }
+        else{
+                        System.out.println(c);
+
+        }
+    }
 }
