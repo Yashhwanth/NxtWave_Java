78d0701c825d8b4d4d8688b33ea4607d34b31195
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 18:56:07 +0530

    Update Eligibility Criteria - 4.java

--- Operators and Conditional Statements/Assignment - 2/Eligibility Criteria - 4.java
+++ Operators and Conditional Statements/Assignment - 2/Eligibility Criteria - 4.java
@@ -1,5 +1,13 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int p = input.nextInt();
+        int c = input.nextInt();
+        if (((m >=60 && p >=60 && c >= 60) && (m + p + c) >=180) || ((m + p >=120) || (c + p >=110))){
+            System.out.println("True");
+        }else System.out.println("False");
+    }
 }
