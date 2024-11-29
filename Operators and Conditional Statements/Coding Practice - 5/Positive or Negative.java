e0343d45581256c1fbb221d13b3ecb4b0d2cc77a
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 22:59:55 +0530

    Update Positive or Negative.java

--- Operators and Conditional Statements/Coding Practice - 5/Positive or Negative.java
+++ Operators and Conditional Statements/Coding Practice - 5/Positive or Negative.java
@@ -1,5 +1,8 @@
-import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(num<0 ? "Negative" : (num==0) ? "Zero" : "Positive");
+    }
 }
