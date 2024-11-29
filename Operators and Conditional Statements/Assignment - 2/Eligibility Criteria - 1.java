741a50c1291c1ba67f562d9399e07b7845d269b7
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 18:54:15 +0530

    Update Eligibility Criteria - 1.java

--- Operators and Conditional Statements/Assignment - 2/Eligibility Criteria - 1.java
+++ Operators and Conditional Statements/Assignment - 2/Eligibility Criteria - 1.java
@@ -1,5 +1,11 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        Integer maths = input.nextInt();
+        Integer physics = input.nextInt();
+        Integer chemistry = input.nextInt();
+        System.out.println((maths >= 70 && physics >=60 && chemistry >=60) ? "True" : (((maths + physics + chemistry) >= 180) ? "True" : "False"));
+    }
 }
