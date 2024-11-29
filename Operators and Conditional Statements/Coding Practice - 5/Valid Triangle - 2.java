718c1ef8f5639980169f9a5f634c38c3043e31d0
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 23:03:23 +0530

    Update Valid Triangle - 2.java

--- Operators and Conditional Statements/Coding Practice - 5/Valid Triangle - 2.java
+++ Operators and Conditional Statements/Coding Practice - 5/Valid Triangle - 2.java
@@ -1,5 +1,14 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int side1 = input.nextInt();
+        int side2 = input.nextInt();
+        int side3 = input.nextInt();
+        if(((side1 + side2) > side3) && ((side3 + side2) > side1) && ((side1 + side3) > side2)){
+            System.out.println("It's a Triangle");
+        }else{
+            System.out.println("It's not a Triangle");
+        }
+    }
 }
