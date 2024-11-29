36a5147b7bfeb5ba166bc7bd97c06c206dbe8f82
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-29 18:53:39 +0530

    Update Masking  - 2.java

--- Operators and Conditional Statements/Assignment - 2/Masking  - 2.java
+++ Operators and Conditional Statements/Assignment - 2/Masking  - 2.java
@@ -1,5 +1,10 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        Integer size = string.length();
+        System.out.println(string.substring(0,2) + "*".repeat(size-4) + string.substring(size-2));
+    }
 }
