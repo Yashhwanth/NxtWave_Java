7978d129df891e32fd045477bb08d2ae346682f5
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-30 14:49:19 +0530

    Update Lucky Number - 2.java

--- Operators and Conditional Statements/Coding Practice - 6/Lucky Number - 2.java
+++ Operators and Conditional Statements/Coding Practice - 6/Lucky Number - 2.java
@@ -1,5 +1,8 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Integer number = input.nextInt();
+        System.out.println(((number % 6 == 0) ? ("Number is divisible by 6") : ((number % 3 == 0) ? ("Number is divisible by 3") : ((number % 2 == 0) ? ("Number is divisible by 2") : ("Number is not divisible by 2, 3 or 6")))));
+    }
 }
