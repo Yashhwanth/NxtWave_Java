92bdbe0020196b84d5ca08bcc532bb8b29b72502
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-30 14:44:51 +0530

    Update Sum or Product.java

--- Operators and Conditional Statements/Assignment - 3/Sum or Product.java
+++ Operators and Conditional Statements/Assignment - 3/Sum or Product.java
@@ -1,5 +1,9 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println((num1 + num2) < 10 ? (num1 + num2):(num1 * num2));
+    }
 }
