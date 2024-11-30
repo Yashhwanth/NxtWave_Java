e0f09f76bf1a389320ab226c0ad3e814b3899ef7
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-30 14:48:40 +0530

    Update Special Number.java

--- Operators and Conditional Statements/Coding Practice - 6/Special Number.java
+++ Operators and Conditional Statements/Coding Practice - 6/Special Number.java
@@ -1,5 +1,16 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String number_str = input.nextLine();
+        int number = Integer.parseInt(number_str);
+        char first_digit_str = number_str.charAt(0);
+        char second_digit_str = number_str.charAt(1);
+        int first_digit = Character.getNumericValue(first_digit_str);
+        int second_digit = Character.getNumericValue(second_digit_str);
+        if(((first_digit + second_digit) == 7) || (first_digit == 7 || second_digit == 7) || (number % 7 == 0)){
+            System.out.println("Special Number");
+        }else   System.out.println("Normal Number");
+
+    }
 }
