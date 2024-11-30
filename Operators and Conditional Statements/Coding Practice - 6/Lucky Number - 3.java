15df1247659e3cc5a1a8339dd09f2c96ae9fb0e1
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-11-30 14:49:54 +0530

    Update Lucky Number - 3.java

--- Operators and Conditional Statements/Coding Practice - 6/Lucky Number - 3.java
+++ Operators and Conditional Statements/Coding Practice - 6/Lucky Number - 3.java
@@ -1,5 +1,16 @@
 import java.util.*;
-class main{
-public static void main(String[] args){
-System.out.println("Hello World");}
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String number_str = input.nextLine();
+        Integer number = Integer.parseInt(number_str);
+        char first_char = number_str.charAt(0);
+        char second_char = number_str.charAt(1);
+        int first_num = Character.getNumericValue(first_char);
+        int second_num = Character.getNumericValue(second_char);
+        if( (number % 9 == 0) || ((first_num == 9) || (second_num == 9))){
+            System.out.println("Lucky Number");
+        }else System.out.println("Unlucky Number");
+
+    }
 }
