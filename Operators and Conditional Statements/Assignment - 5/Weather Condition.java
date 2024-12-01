838f74bc1359a114fd950b71e2beccbc9786f7db
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-01 23:27:59 +0530

    Update Weather Condition.java

--- Operators and Conditional Statements/Assignment - 5/Weather Condition.java
+++ Operators and Conditional Statements/Assignment - 5/Weather Condition.java
@@ -1,8 +1,14 @@
 import java.util.*;
-class Main{
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
         Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        System.out.println((number % 2 == 0) ? ("Even") : ("Odd"));
+        float temp = input.nextFloat();
+        if(temp < 0) System.out.println("Freezing weather");
+        if(temp >= 0 && temp < 10) System.out.println("Very Cold weather");
+        if(temp >= 10 && temp < 20) System.out.println("Cold weather");
+        if(temp >= 20 && temp < 30) System.out.println("Normal");
+        if(temp >= 30 && temp < 40) System.out.println("Hot");
+        if(temp >= 40) System.out.println("Very Hot");
     }
 }
