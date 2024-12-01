160accdaa94e7663f16fe7cd4a696211efdb211b
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-01 21:49:07 +0530

    Update Get Season.java

--- Operators and Conditional Statements/Coding Practice - 7/Get Season.java
+++ Operators and Conditional Statements/Coding Practice - 7/Get Season.java
@@ -2,7 +2,11 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        System.out.println((number % 2 == 0) ? ("Even") : ("Odd"));
+        int season = input.nextInt();
+        if(season == 11 || season == 12 || season == 1) System.out.println("Winter");
+        else if(season == 2 || season == 3 ) System.out.println("Spring");
+        else if(season == 4 || season == 5 || season == 6) System.out.println("Summer");
+        else if(season == 7 || season == 8 ) System.out.println("Rainy");
+        else if(season == 9 || season == 10 || season == 1) System.out.println("Autumn");
     }
 }
