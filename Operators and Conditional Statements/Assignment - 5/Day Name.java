16c6bb43290faa2482921c9f94d2176f69950cf7
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-01 23:25:49 +0530

    Update Day Name.java

--- Operators and Conditional Statements/Assignment - 5/Day Name.java
+++ Operators and Conditional Statements/Assignment - 5/Day Name.java
@@ -1,8 +1,15 @@
 import java.util.*;
-class Main{
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
         Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        System.out.println((number % 2 == 0) ? ("Even") : ("Odd"));
+        int day = input.nextInt();
+        if(day == 1) System.out.println("Monday");
+        if(day == 2) System.out.println("Tuesday");
+        if(day == 3) System.out.println("Wednesday");
+        if(day == 4) System.out.println("Thursday");
+        if(day == 5) System.out.println("Friday");
+        if(day == 6) System.out.println("Saturday");
+        if(day == 7) System.out.println("Sunday");
     }
 }
