d91e5f7537a48f00172b310271eb690e9769cd95
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-03 13:53:56 +0530

    Print Integers - 2.java

---
+++ Loops/Coding Practice - 10/Print Integers - 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        for(int i = m ; i <= n ; i ++){
+            System.out.println(i);
+        }
+    }
+}
