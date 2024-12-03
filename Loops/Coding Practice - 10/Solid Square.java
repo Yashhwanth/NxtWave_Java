33fc58322080211b0b92606bc7ca9fc8371c16f4
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-03 13:59:34 +0530

    Update and rename Hello World - Copy.java to Solid Square.java

---
+++ Loops/Coding Practice - 10/Solid Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = 0 ; j < n ; j ++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
