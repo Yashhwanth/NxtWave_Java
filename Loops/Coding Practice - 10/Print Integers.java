0c81692b67a1e5bb15d02274e17c279e00a24e57
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-03 13:52:18 +0530

    Update and rename Hello World - Copy (2).java to Print Integers.java

---
+++ Loops/Coding Practice - 10/Print Integers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        for(int i = 1 ; i <= number ; i ++){
+            System.out.println(i);
+        }
+    }
+}
