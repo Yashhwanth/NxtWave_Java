1eaadc7a2629749eeadff722b1172f6c90535920
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-05 10:28:21 +0530

    Create Valid Password - 2.java

---
+++ Loops/Coding Practice - 13/Valid Password - 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        int count = 0;
+        for(int i = 0 ; i < str.length() ; i ++){
+            if(Character.isUpperCase(str.charAt(i))) count = count + 1;
+        }System.out.println(count > 0 ? "Valid Password" : "Invalid Password");
+    }
+}
