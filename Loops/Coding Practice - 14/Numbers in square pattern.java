1c086513659d5f503daf1a9a4a76a2e87bd06955
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-05 10:44:19 +0530

    Create Numbers in square pattern.java

---
+++ Loops/Coding Practice - 14/Numbers in square pattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i = 1 ; i <= n ; i ++){
+            for(int j = 1 ; j <= n ; j ++){
+                System.out.print(j + " ");
+            }System.out.println();
+        }
+    }
+}
