fe4d47655085de45ea869dc7d06111d588fe4e76
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-05 10:26:16 +0530

    Create Lowercase&Uppercase.java

---
+++ Loops/Coding Practice - 13/Lowercase&Uppercase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String string = input.nextLine();
+        System.out.println(string.toLowerCase());
+        System.out.println(string.toUpperCase());
+    }
+}
