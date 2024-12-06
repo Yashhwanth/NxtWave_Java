5d4975764db09f78695fbcf05479bee24fdd8ef7
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-06 13:53:36 +0530

    Create Sum of Odd Numbers.java

---
+++ Loops/Assignment - 11/Sum of Odd Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        long sum= 0;
+        for(int i = 1 ; i <= n ; i = i + 2){
+            sum = sum + i;
+        }System.out.println(sum);
+    }
+}
