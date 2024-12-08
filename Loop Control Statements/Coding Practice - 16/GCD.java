db800848820a4208c5640ce85495cd3800791e6a
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-08 10:53:16 +0530

    Update GCD.java

--- Loop Control Statements/Coding Practice - 16/GCD.java
+++ Loop Control Statements/Coding Practice - 16/GCD.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        int small_number = m > n ? n : m;
+        int gcd = 1;
+        for(int i = 2 ; i <= small_number ; i ++){
+            if(m % i == 0 && n % i == 0){
+                gcd = i;
+            }
+        }System.out.println(gcd);
+    }
+}
