29f112fc52dda92c7910303ca8161f2f5df0276f
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-10 19:54:20 +0530

    Create Kth largest factor of N.java

---
+++ Loop Control Statements/Assignment - 15/Kth largest factor of N.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        int fac = 0;
+        for(int i = n ; i > 0 ; i --){
+            if(n % i == 0){
+                fac ++;
+                if(fac == k){
+                    System.out.println(i);
+                    break;
+                } 
+            }
+        }
+        if(fac < k) System.out.println(1);
+    }
+}
