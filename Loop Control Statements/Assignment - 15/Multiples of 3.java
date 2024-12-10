9d44a2ede2735227c87ad7293f86ef9d65718bb5
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-10 19:56:46 +0530

    Create Multiples of 3.java

---
+++ Loop Control Statements/Assignment - 15/Multiples of 3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i = 0 ; i < n ; i ++){
+            int x = input.nextInt();
+            if(x % 3 == 0) System.out.println(x);
+        }
+    }
+}
