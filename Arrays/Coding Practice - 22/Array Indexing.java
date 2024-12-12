1ec9202d3a7a421f858546430e3bbd82c65c1f03
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-12 23:05:04 +0530

    Create Array Indexing.java

---
+++ Arrays/Coding Practice - 22/Array Indexing.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String[] arr = {"Red", "Orange", "Yellow", "Pink", "Green", "Blue", "Purple", "Black", "White"};
+        int n = input.nextInt();
+        System.out.println(arr[n]);    }
+    
+}
