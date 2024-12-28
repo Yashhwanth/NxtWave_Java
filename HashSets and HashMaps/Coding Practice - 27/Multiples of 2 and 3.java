22a8bf52d080256bdc8b131e1eb5a33724d12df5
Yashwanth K <137391328+Yashhwanth@users.noreply.github.com>
2024-12-28 21:49:14 +0530

    Update Multiples of 2 and 3.java

--- HashSets and HashMaps/Coding Practice - 27/Multiples of 2 and 3.java
+++ HashSets and HashMaps/Coding Practice - 27/Multiples of 2 and 3.java
@@ -5,7 +5,6 @@ class main{
         int num = input.nextInt();
         HashSet<Integer> two_mul = new HashSet<Integer>();
         HashSet<Integer> three_mul = new HashSet<Integer>();
-        
         for(int i = 1 ; i <= num  ; i++){
             two_mul.add(2 * i);
             three_mul.add(3 * i);
@@ -17,14 +16,12 @@ class main{
         two_but_not_three.removeAll(three_mul);
         ArrayList<Integer> list1 = new ArrayList<>(two_but_not_three);
         Collections.sort(list1);
-        
         HashSet<Integer> uncommon_mul = new HashSet<Integer>();
         uncommon_mul.addAll(two_mul);
         uncommon_mul.addAll(three_mul);
         uncommon_mul.removeAll(common_mul);
         ArrayList<Integer> list2 = new ArrayList<>(uncommon_mul);
         Collections.sort(list2);
-        
         System.out.println(list1);
         System.out.println(list2);
 
